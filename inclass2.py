38ebe47b7f9a65562a6f6add5a7616c7e436dd40
lynn-sketch <110109942+lynn-sketch@users.noreply.github.com>
2023-10-12 09:54:34 +0100

    inclass2.py

---
+++ inclass2.py
@@ -0,0 +1,14 @@
+class MyClass:
+    pass
+
+
+Object1 = MyClass()
+Object2 = MyClass()
+
+# Giving us memory address
+print(Object1)
+print(Object2)
+
+# giving us the name of the object
+print(type(Object1))
+print(type(Object2))
