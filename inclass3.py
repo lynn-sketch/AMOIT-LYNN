028af0d1984b1a1bbdc1ec0cf2d44d3113aae0b8
lynn-sketch <110109942+lynn-sketch@users.noreply.github.com>
2023-10-12 09:57:10 +0100

    inclass3.py

---
+++ inclass3.py
@@ -0,0 +1,34 @@
+# class MyClass:
+#     pass
+
+
+# Object1 = MyClass()
+# Object2 = MyClass()
+
+# # Giving us memory address
+# print(Object1)
+# print(Object2)
+
+# # giving us the name of the object
+# print(type(Object1))
+# print(type(Object2))
+
+
+
+ class Myclass:
+    def __int__(self):
+        self.color = "green"
+        self.fruit = "orange"
+
+fruit2 = Myclass()
+print(fruit2.color)
+print(fruit2.fruit)
+
+# fruit2 = Myclass()
+# fruit2.color = 'yellow'
+# fruit2.name
+
+
+# class client:
+#     def __init(self)
+
